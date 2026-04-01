71c212296237b5a6bb5994cee9aee8df92f006f3
Debangaraj Kaushik <debangaraj.kaushik@example.com>
2026-04-02 02:39:58 +0530

    Extend Tailwind theme

--- frontend/react-app/tailwind.config.cjs
+++ frontend/react-app/tailwind.config.cjs
@@ -1,7 +1,22 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Outfit', ...defaultTheme.fontFamily.sans],
+        mono: ['"JetBrains Mono"', ...defaultTheme.fontFamily.mono],
+      },
+      colors: {
+        space: '#080C14',
+      },
+      keyframes: {
+        shimmer: {
+          '100%': { transform: 'translateX(300%) skew(-20deg)' },
+        }
+      }
+    },
   },
   plugins: [],
 }
